ca18320bd42723854c4dac26860f436f50a51aa6
bala-amypo <bala@amypo.in>
2025-12-19 09:46:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+public class RegisterRequest {
+
+    private String employeeid;
+    private String username;
+    private String email;
+    private String password;
+    private String role;     // ADMIN, USER, AUDITOR
+
+    public RegisterRequest() {}
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
