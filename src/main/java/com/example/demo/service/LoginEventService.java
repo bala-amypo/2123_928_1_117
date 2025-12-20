9846cab0659764a1d158d901690ad733f0d3e9be
bala-amypo <bala@amypo.in>
2025-12-20 04:19:37 +0000

    User commit

--- src/main/java/com/example/demo/service/LoginEventService.java
+++ src/main/java/com/example/demo/service/LoginEventService.java
@@ -1,11 +1,33 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.LoginEvent;
-import com.example.demo.entity.LoginEvent.LoginStatus;
-
 import java.util.List;
 
-public interface LoginEventService {
+import org.springframework.stereotype.Service;
 
-    List<LoginEvent> getEventsByStatus(LoginStatus status);
+import com.example.demo.entity.LoginEvent;
+import com.example.demo.repository.LoginEventRepository;
+
+@Service
+public class LoginEventService {
+
+    private final LoginEventRepository loginEventRepository;
+
+    public LoginEventService(LoginEventRepository loginEventRepository) {
+        this.loginEventRepository = loginEventRepository;
+    }
+
+    public LoginEvent save(LoginEvent loginEvent) {
+        return loginEventRepository.save(loginEvent);
+    }
+
+    public List<LoginEvent> getUserLoginEvents(Long userId) {
+        return loginEventRepository.findByUserId(userId);
+    }
+
+    public List<LoginEvent> getUserFailedLogins(Long userId) {
+        return loginEventRepository.findByUserIdAndStatus(
+                userId,
+                LoginEvent.LoginStatus.FAILED
+        );
+    }
 }
