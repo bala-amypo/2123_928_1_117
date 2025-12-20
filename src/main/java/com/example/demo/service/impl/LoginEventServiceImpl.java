175e22d91608926c37bc517ac9fd6835db29aaa2
bala-amypo <bala@amypo.in>
2025-12-20 03:40:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoginEventServiceImpl.java
@@ -4,30 +4,21 @@ import com.example.demo.entity.LoginEvent;
 import com.example.demo.entity.LoginEvent.LoginStatus;
 import com.example.demo.repository.LoginEventRepository;
 import com.example.demo.service.LoginEventService;
-
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Service
 public class LoginEventServiceImpl implements LoginEventService {
 
-    @Autowired
-    private LoginEventRepository loginRepo;
+    private final LoginEventRepository repository;
 
-    @Override
-    public LoginEvent save(LoginEvent event) {
-        return loginRepo.save(event);
+    public LoginEventServiceImpl(LoginEventRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public List<LoginEvent> getUserLoginEvents(Long userid) {
-        return loginRepo.findByUserid(userid);
-    }
-
-    @Override
-    public List<LoginEvent> getUserFailedLogins(Long userid) {
-        return loginRepo.findByUseridAndLoginStatus(userid, LoginStatus.FAILED);
+    public List<LoginEvent> getEventsByStatus(LoginStatus status) {
+        return repository.findByStatus(status);
     }
 }
